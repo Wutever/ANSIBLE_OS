1f9a36199a3243011a0066b76463cd3f91b14945
root <root@object.ncsa.illinois.edu>
2019-09-18 10:03:59 -0500

    adding script

---
+++ test.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+IPTABLES=/sbin/iptables
+
+WANIF='enp4s0'
+LANIF='enp8s0'
+
+# enable ip forwarding in the kernel
+echo 'Enabling Kernel IP forwarding...'
+/bin/echo 1 > /proc/sys/net/ipv4/ip_forward
+/sbin/sysctl -w net.ipv4.ip_forward=1
+
+# flush rules and delete chains
+echo 'Flushing rules and deleting existing chains...'
+$IPTABLES -F
+$IPTABLES -X
+
+# enable masquerading to allow LAN internet access
+echo 'Enabling IP Masquerading and other rules...'
+$IPTABLES -t nat -A POSTROUTING -o $LANIF -j MASQUERADE
+$IPTABLES -A FORWARD -i $LANIF -o $WANIF -m state --state RELATED,ESTABLISHED -j ACCEPT
+$IPTABLES -A FORWARD -i $WANIF -o $LANIF -j ACCEPT
+
+$IPTABLES -t nat -A POSTROUTING -o $WANIF -j MASQUERADE
+$IPTABLES -A FORWARD -i $WANIF -o $LANIF -m state --state RELATED,ESTABLISHED -j ACCEPT
+$IPTABLES -A FORWARD -i $LANIF -o $WANIF -j ACCEPT
+~
+~
+~
+~
+~
+~
+
